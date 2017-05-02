3e1ee16f134e2b0d1e468d7f7949e7332a75d0d8
Guillermo Tamanaha <guillermo.tamanaha@outlook.com>
2017-05-02 14:40:46 -0300

    removing angularfire2

--- site/src/app/dashboard/table/table.component.ts
+++ site/src/app/dashboard/table/table.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, AfterViewInit, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 import { UserService } from '../services/user.service';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
     moduleId: module.id,
@@ -53,23 +52,7 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 })
 
 export class TableComponent implements OnInit {
-    users: FirebaseListObservable<any[]>;
-    constructor(private af: AngularFire) { }
 
     ngOnInit() {
-        this.users = this.af.database.list('/users', {
-            query: {
-                orderByKey: true,
-                limitToFirst: 10,
-                startAt: '1'
-            }
-        });
-
-        //console.log(this.users);
-        console.log(this.users);
-
-        this.users.subscribe(function () {
-            console.log("done???")
-        });
     }
 }
